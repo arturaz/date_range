6f135bb842a22b10def02fad33f7a84fcd1c490b
Arturas Slajus <x11@arturaz.net>
2008-10-19 03:27:42 +0300

    Initial import.

---
+++ lib/date_range.rb
@@ -0,0 +1,132 @@
+module DateRange
+  class << self
+    DATE_REGEXP = %r{
+      ^\s*
+      (
+        (\d{4})
+        (-(\d{1,2})
+          (-(\d{1,2})
+            (\s+(\d{1,2})
+              (:(\d{1,2})
+                (:(\d{1,2}))?
+              )?
+            )?
+          )?
+        )?
+      )
+      \s*\-?\s*
+      (
+        (\d{4})
+        (-(\d{1,2})
+          (-(\d{1,2})
+            (\s+(\d{1,2})
+              (:(\d{1,2})
+                (:(\d{1,2}))?
+              )?
+            )?
+          )?
+        )?
+      )?
+      \s*$
+    }x
+
+    # Parse out _str_ and return two Time objects.
+    #
+    # It will return nil if one of the times are corrupt (like 13th month)
+    # or missing.
+    #
+    # It will also look at context if no end time is given so:
+    # * <tt>2008</tt> becomes 2008-01-01 00:00:00 - 2008-12-31 23:59:59
+    # * <tt>2008-01</tt> becomes 2008-01-01 00:00:00 - 2008-01-31 23:59:59
+    def parse(str)
+      match_data = str.match(DATE_REGEXP)
+      # Oh my god, I hate this shit. Basically it prepares dates for 
+      # Time.parse consuming because it's so stupid and ignorant it wants
+      # to make me puke :(
+      from = extract_time_string_from_match_data(match_data)
+      to = extract_time_string_from_match_data(match_data, 12)
+
+      begin
+        from = Time.parse(from) unless from.nil?
+      rescue ArgumentError
+        from = nil
+      end
+
+      begin
+        if to.nil? and not from.nil?
+          case extract_context_from_match_data(match_data)
+          when :year
+            to = from.end_of_year
+          when :month
+            to = from.end_of_month
+          end 
+        else
+          to = Time.parse(to)
+        end
+      rescue ArgumentError
+        to = nil
+      end
+
+      [from, to]
+    end
+
+    # Check if _str_ is a date range.
+    #
+    # Date range format is ([] indicating optional parts):
+    #
+    # <code>yyyy[-MM[-dd[ hh[:mm[:ss]]]]][ - ][yyyy[-MM[-dd[ hh[:mm[:ss]]]]]]</code>
+    def match?(str)
+      str.match(DATE_REGEXP).nil? ? false : true
+    end
+
+    private
+    # Extract context from match data. For example if user enters just
+    # 2008 it will return :year.
+    #
+    # Return value meanings:
+    #
+    # * <tt>:year</tt>:      end range should be years end
+    # * <tt>:month</tt>:     end range should be months end
+    # * <tt>:nothing</tt>:   nothing should be done with end range
+    def extract_context_from_match_data(m, offset=0)
+      year, month, day, hour, minute, second = \
+        extract_pieces_from_match_data(m, offset)
+
+      if year and month and day
+        :nothing
+      elsif year and month
+        :month
+      elsif year
+        :year
+      end
+    end
+
+    def extract_time_string_from_match_data(m, offset=0)
+      pieces = extract_pieces_from_match_data(m, offset)
+
+      # If nothing matched - there was no date.
+      if pieces.compact.blank?
+        nil
+      else
+        # month and day are required to start at 1
+        pieces[1] ||= 1
+        pieces[2] ||= 1
+        # Convert everything to integer
+        pieces.map!(&:to_i)
+
+        "%04d-%02d-%02d %02d:%02d" % pieces
+      end
+    end
+    
+    def extract_pieces_from_match_data(m, offset=0)
+      [
+        m[2 + offset],  # year
+        m[4 + offset],  # month
+        m[6 + offset],  # day
+        m[8 + offset],  # hour
+        m[10 + offset], # minute
+        m[12 + offset]  # seconds
+      ]
+    end
+  end
+end
---
+++ spec/date_range_spec.rb
@@ -0,0 +1,69 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe DateRange do
+  describe ".match?" do
+    it "should return true if supplied date" do
+      DateRange.match?("2006-7-12 14:30:15 - 2008").should be_true
+      DateRange.match?("2006-7-12 14:30:15").should be_true
+      DateRange.match?("2006-7-12 14:30").should be_true
+      DateRange.match?("2006-7-12 14").should be_true
+      DateRange.match?("2006-7-12").should be_true
+      DateRange.match?("2006-7").should be_true
+      DateRange.match?("2006").should be_true 
+    end
+    
+    it "should return false if suplied nonsense" do
+      DateRange.match?("asdas").should be_false
+    end
+  end
+  
+  describe ".parse" do
+    it "should make year be equal to year-end of year" do
+      time = Time.parse('2008-01-01')
+      DateRange.parse("#{time.year}").should eql(
+        [time, time.end_of_year])
+    end
+    
+    it "should make year-month be equal to year-month - end of month" do
+      time = Time.parse('2008-02-01')
+      DateRange.parse("2008-02").should eql(
+        [time, time.end_of_month])
+    end
+    
+    it "should not fail on 2008-09-19" do
+      lambda do
+        DateRange.parse("2008-09-19")
+      end.should_not raise_error
+    end
+    
+    it "should return [Time, nil] on single date" do
+      from, to = DateRange.parse("2004-05-06")
+      from.should be_instance_of(Time)
+      to.should be_nil
+    end
+    
+    it "should return 2 Time objects on correct range" do
+      from, to = DateRange.parse("2004-05-06 2004-10-10")
+      from.should be_instance_of(Time)
+      to.should be_instance_of(Time)
+    end
+    
+    it "should return [nil, Time] if from is broken" do
+      from, to = DateRange.parse("2004-13-06 2004-10-10")
+      from.should be_nil
+      to.should be_instance_of(Time)
+    end
+    
+    it "should return [Time, nil] if end is broken" do
+      from, to = DateRange.parse("2004-05-06 2004-13-10")
+      from.should be_instance_of(Time)
+      to.should be_nil
+    end
+    
+    it "should return [nil, nil] if everything is fucked up" do
+      from, to = DateRange.parse("2004-13-06 2004-14-10")
+      from.should be_nil
+      to.should be_nil
+    end
+  end
+end
